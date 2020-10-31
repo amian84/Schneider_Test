535abe84ed5bdfc4fb5fd641cde2c20161013286
David Amian <amian84@gmail.com>
2020-10-31 14:39:32 +0100

    Create SOAP response class

---
+++ SchneiderTestBack/ORM/ResponseMessages/SoapListElectricityMeter.cs
@@ -0,0 +1,17 @@
+﻿
+using ORM.Model;
+using System.Collections.Generic;
+
+namespace ORM.ResponseMessages
+{
+    /// <summary>
+    /// Class to set soap message
+    /// </summary>
+    public class SoapListElectricityMeter : SOAPResponse
+    {
+        /// <summary>
+        /// List of entities
+        /// </summary>
+        public List<ElectricityMeter> Entities { get; set; }
+    }
+}
---
+++ SchneiderTestBack/ORM/ResponseMessages/SoapListGateway.cs
@@ -0,0 +1,20 @@
+﻿using ORM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.ResponseMessages
+{
+    /// <summary>
+    /// Class to set soap message
+    /// </summary>
+    public class SoapListGateway: SOAPResponse
+    {
+        /// <summary>
+        /// List of entities
+        /// </summary>
+        public List<Gateway> Entities { get; set; }
+    }
+}
