fa48feccde5520ef6cb2a060136b9b5202407d50
David Amian <amian84@gmail.com>
2020-10-31 14:39:41 +0100

    Create SOAPResponse.cs

---
+++ SchneiderTestBack/ORM/ResponseMessages/SOAPResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.ResponseMessages
+{
+    /// <summary>
+    /// Class to set soap message
+    /// </summary>
+    public class SOAPResponse
+    {
+        /// <summary>
+        /// Code of soap return message
+        /// </summary>
+        public int Code { get; set; }
+        // <summary>
+        /// Message to return in soap response
+        /// </summary>
+        public string Msg { get; set; }
+    }
+}
