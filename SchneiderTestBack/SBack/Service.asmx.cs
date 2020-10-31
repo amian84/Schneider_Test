4a1e37b141c814e426b5e3f1a601a9218dc68ee7
David Amian <amian84@gmail.com>
2020-10-31 14:40:09 +0100

    Added all methods

--- SchneiderTestBack/SBack/Service.asmx.cs
+++ SchneiderTestBack/SBack/Service.asmx.cs
@@ -1,8 +1,6 @@
 ﻿using ORM;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using ORM.Model;
+using ORM.ResponseMessages;
 using System.Web.Services;
 
 namespace SBack
@@ -15,15 +13,70 @@ namespace SBack
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
     // [System.Web.Script.Services.ScriptService]
-    public class Service : System.Web.Services.WebService
+    public class Service : WebService
     {
 
         [WebMethod]
-        public string HelloWorld(string text)
+        public SOAPResponse CreateWaterMeter(string serial, string brand, string model)
         {
-            Class1 cl = new Class1();
-            cl.createTest(text);
-            return "Hola a todos";
+            return DBManager.Get().CreateEntity(typeof(WaterMeter),serial, brand, model);
         }
+
+        [WebMethod]
+        public SOAPResponse CreateElectricityMeter(string serial, string brand, string model)
+        {
+            return DBManager.Get().CreateEntity(typeof(ElectricityMeter), serial, brand, model);
+        }
+
+        [WebMethod]
+        public SOAPResponse CreateGateway(string serial, string brand, string model, string ip, string port)
+        {
+            int? portConverted = null;
+            if (!string.IsNullOrEmpty(port))
+            {
+                int portInt;
+                int.TryParse(port, out portInt);
+                portConverted = portInt;
+            }
+            return DBManager.Get().CreateEntity(typeof(Gateway), serial, brand, model, ip, portConverted);
+        }
+
+        [WebMethod]
+        public SoapListGateway GetAllGateways()
+        {
+            return DBManager.Get().GetAllGWEntities();
+        }
+
+        [WebMethod]
+        public SoapListGateway GetGatewayBySerial(string serial)
+        {
+            return DBManager.Get().GetAllGWEntities(serial);
+        }
+
+        [WebMethod]
+        public SoapListElectricityMeter GetAllElectricityMeter()
+        {
+            return DBManager.Get().GetAllElectricityEntities();
+        }
+
+        [WebMethod]
+        public SoapListElectricityMeter GetElectricityMeterBySerial(string serial)
+        {
+            return DBManager.Get().GetAllElectricityEntities(serial);
+        }
+
+
+        [WebMethod]
+        public SoapListWaterMeter GetAllWaterMeter()
+        {
+            return DBManager.Get().GetAllWaterEntities();
+        }
+
+        [WebMethod]
+        public SoapListWaterMeter GetWaterMeterBySerial(string serial)
+        {
+            return DBManager.Get().GetAllWaterEntities(serial);
+        }
+
     }
 }
