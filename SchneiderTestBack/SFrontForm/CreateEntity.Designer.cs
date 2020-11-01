be825a85de30cd7638e3123de164bcc7d09bfde3
David Amian <amian84@gmail.com>
2020-11-01 10:14:24 +0100

    Create form to create any entity

---
+++ SchneiderTestBack/SFrontForm/CreateEntity.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SFrontForm
+{
+    partial class CreateEntity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSN = new System.Windows.Forms.TextBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lbSN = new System.Windows.Forms.Label();
+            this.lbBrand = new System.Windows.Forms.Label();
+            this.lbModel = new System.Windows.Forms.Label();
+            this.lbIp = new System.Windows.Forms.Label();
+            this.lbPort = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSN
+            // 
+            this.txtSN.Location = new System.Drawing.Point(120, 20);
+            this.txtSN.Name = "txtSN";
+            this.txtSN.Size = new System.Drawing.Size(249, 22);
+            this.txtSN.TabIndex = 0;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(120, 69);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(249, 22);
+            this.txtBrand.TabIndex = 1;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(120, 120);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(249, 22);
+            this.txtModel.TabIndex = 2;
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(120, 176);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(145, 22);
+            this.txtIp.TabIndex = 3;
+            this.txtIp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIp_KeyPress);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(326, 176);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(43, 22);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPort_KeyPress);
+            // 
+            // lbSN
+            // 
+            this.lbSN.AutoSize = true;
+            this.lbSN.Location = new System.Drawing.Point(12, 23);
+            this.lbSN.Name = "lbSN";
+            this.lbSN.Size = new System.Drawing.Size(102, 17);
+            this.lbSN.TabIndex = 5;
+            this.lbSN.Text = "Serial Number:";
+            // 
+            // lbBrand
+            // 
+            this.lbBrand.AutoSize = true;
+            this.lbBrand.Location = new System.Drawing.Point(12, 74);
+            this.lbBrand.Name = "lbBrand";
+            this.lbBrand.Size = new System.Drawing.Size(50, 17);
+            this.lbBrand.TabIndex = 6;
+            this.lbBrand.Text = "Brand:";
+            // 
+            // lbModel
+            // 
+            this.lbModel.AutoSize = true;
+            this.lbModel.Location = new System.Drawing.Point(12, 123);
+            this.lbModel.Name = "lbModel";
+            this.lbModel.Size = new System.Drawing.Size(50, 17);
+            this.lbModel.TabIndex = 7;
+            this.lbModel.Text = "Model:";
+            // 
+            // lbIp
+            // 
+            this.lbIp.AutoSize = true;
+            this.lbIp.Location = new System.Drawing.Point(12, 181);
+            this.lbIp.Name = "lbIp";
+            this.lbIp.Size = new System.Drawing.Size(23, 17);
+            this.lbIp.TabIndex = 8;
+            this.lbIp.Text = "Ip:";
+            // 
+            // lbPort
+            // 
+            this.lbPort.AutoSize = true;
+            this.lbPort.Location = new System.Drawing.Point(282, 179);
+            this.lbPort.Name = "lbPort";
+            this.lbPort.Size = new System.Drawing.Size(38, 17);
+            this.lbPort.TabIndex = 9;
+            this.lbPort.Text = "Port:";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(289, 236);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(80, 33);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(184, 236);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 33);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // CreateEntity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 297);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lbPort);
+            this.Controls.Add(this.lbIp);
+            this.Controls.Add(this.lbModel);
+            this.Controls.Add(this.lbBrand);
+            this.Controls.Add(this.lbSN);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtSN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CreateEntity";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CreateEntity";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSN;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lbSN;
+        private System.Windows.Forms.Label lbBrand;
+        private System.Windows.Forms.Label lbModel;
+        private System.Windows.Forms.Label lbIp;
+        private System.Windows.Forms.Label lbPort;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ SchneiderTestBack/SFrontForm/CreateEntity.cs
@@ -0,0 +1,204 @@
+﻿using SFrontForm.SBack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SFrontForm
+{
+    public struct ResponseSOAP
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+    public partial class CreateEntity : Form
+    {
+        private ServiceSoap Soap;
+        private Type EntType;
+        private Form1 ParentFormFront;
+        public CreateEntity(Form1 parentForm)
+        {
+            InitializeComponent();
+            Soap = new ServiceSoapClient();
+            ParentFormFront = parentForm;
+        }
+
+        /// <summary>
+        /// Set type of entity to create.
+        /// Check type and enable ip and port if is GW
+        /// </summary>
+        /// <param name="ent">Entity type, can be (Gateway, ElectricityMeter or WaterMeter)</param>
+        public void SetEntityType(Type ent)
+        {
+            EntType = ent;
+            bool visible = false;
+            if (ent == typeof(Gateway))
+            {
+                visible = true;
+            }
+            lbIp.Visible = visible;
+            lbPort.Visible = visible;
+            txtIp.Visible = visible;
+            txtPort.Visible = visible;
+        }
+
+        /// <summary>
+        /// Close windows
+        /// </summary>
+        /// <param name="sender">button control</param>
+        /// <param name="e">Arguments for event that fire the click button</param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+
+        /// <summary>
+        /// Get data from all textbox and call to SOAP back to create
+        /// the entity depends of entity type variable
+        /// </summary>
+        /// <param name="sender">button control</param>
+        /// <param name="e">Arguments for event that fire the click button</param>
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string serialNumber = txtSN.Text;
+            string brand = txtBrand.Text;
+            string model = txtModel.Text;
+            string ip = txtIp.Text;
+            string port = txtPort.Text;
+            ResponseSOAP response = new ResponseSOAP();
+            response.Code = 500;
+            response.Message = "Can't create entity for unkonwn entity";
+            if (EntType == typeof(Gateway))
+            {
+                response = CreateGW(serialNumber, brand, model, ip, port);
+            }
+            else if (EntType == typeof(ElectricityMeter))
+            {
+                response = CreateEM(serialNumber, brand, model);
+            }
+            else if (EntType == typeof(WaterMeter))
+            {
+                response = CreateWM(serialNumber, brand, model);
+            }
+            if (response.Code != 200)
+            {
+                MessageBox.Show(
+                    "Error creating entity: " + response.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            else
+            {
+                ParentFormFront.RefreshGV(EntType);
+                this.Close();
+                this.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        /// Call Soap method to create Water Meter entity
+        /// </summary>
+        /// <param name="serialNumber">Serial Number</param>
+        /// <param name="brand">Brand </param>
+        /// <param name="model">Model</param>
+        /// <returns>ResponseSOAP object with Soap response </returns>
+        private ResponseSOAP CreateWM(string serialNumber, string brand, string model)
+        {
+            ResponseSOAP response = new ResponseSOAP();
+            CreateWaterMeterRequest rq = new CreateWaterMeterRequest();
+            rq.Body = new CreateWaterMeterRequestBody();
+            rq.Body.serial = serialNumber;
+            rq.Body.brand = brand;
+            rq.Body.model = model;
+
+            CreateWaterMeterResponse res = Soap.CreateWaterMeter(rq);
+            response.Code = res.Body.CreateWaterMeterResult.Code;
+            response.Message = res.Body.CreateWaterMeterResult.Msg;
+            return response;
+        }
+
+        /// <summary>
+        /// Call Soap method to create Electricity Meter entity
+        /// </summary>
+        /// <param name="serialNumber">Serial Number</param>
+        /// <param name="brand">Brand </param>
+        /// <param name="model">Model</param>
+        /// <returns>ResponseSOAP object with Soap response </returns>
+        private ResponseSOAP CreateEM(string serialNumber, string brand, string model)
+        {
+            ResponseSOAP response = new ResponseSOAP();
+            CreateElectricityMeterRequest rq = new CreateElectricityMeterRequest();
+            rq.Body = new CreateElectricityMeterRequestBody();
+            rq.Body.serial = serialNumber;
+            rq.Body.brand = brand;
+            rq.Body.model = model;
+
+            CreateElectricityMeterResponse res = Soap.CreateElectricityMeter(rq);
+            response.Code = res.Body.CreateElectricityMeterResult.Code;
+            response.Message = res.Body.CreateElectricityMeterResult.Msg;
+            return response;
+        }
+
+        /// <summary>
+        /// Call Soap method to create Gateway entity
+        /// </summary>
+        /// <param name="serialNumber">Serial Number</param>
+        /// <param name="brand">Brand </param>
+        /// <param name="model">Model</param>
+        /// <param name="ip">IP</param>
+        /// <param name="port">Port</param>
+        /// <returns>ResponseSOAP object with Soap response </returns>
+        private ResponseSOAP CreateGW(string serialNumber, string brand, string model, string ip, string port)
+        {
+            ResponseSOAP response = new ResponseSOAP();
+            CreateGatewayRequest rq = new CreateGatewayRequest();
+            rq.Body = new CreateGatewayRequestBody();
+            rq.Body.serial = serialNumber;
+            rq.Body.brand = brand;
+            rq.Body.model = model;
+            rq.Body.ip = ip;
+            rq.Body.port = port;
+            CreateGatewayResponse res = Soap.CreateGateway(rq);
+            response.Code = res.Body.CreateGatewayResult.Code;
+            response.Message = res.Body.CreateGatewayResult.Msg;
+            return response;
+        }
+
+        /// <summary>
+        ///  Only allow numbers and dots in textbox for IP
+        /// </summary>
+        /// <param name="sender">text box control</param>
+        /// <param name="e">key pressed event</param>
+        private void txtIp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Only allow numbers in textbox for Port
+        /// </summary>
+        /// <param name="sender">text box control</param>
+        /// <param name="e">key pressed event</param>
+        private void txtPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
